e6f3125aa528296d8ed69eee6c8c450d09b7038d
Aranya Varatharajan <aranya.v@volantetech.com>
2025-08-19 14:56:58 +0530

    Committing 27_RemoveElement initial solution

--- problems/27_RemoveElement/src/Solution.java
+++ problems/27_RemoveElement/src/Solution.java
@@ -1,5 +1,33 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
 
+    public int removeElement(int[] nums, int val) {
+        int numsLength = nums.length;
+        int i = 0, j = numsLength - 1;
+        if(numsLength==0) return 0;
+        if (numsLength == 1) {
+            if (nums[0] == val) {
+                nums[0] = -1;
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        while (i < nums.length && i <= j) {
+            if (nums[i] == val) {
+                if (nums[j] == val) {
+                    nums[j] = -1;
+                    j--;
+                } else {
+                    nums[i] = nums[j];
+                    nums[j] = -1;
+                    j--;
+                    i++;
+                }
+            } else {
+                i++;
+            }
+
+        }
+        return j + 1;
     }
 }
