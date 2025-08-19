2e4f876965ea9b487be43e7f8c4101e293038132
Aranya Varatharajan <aranya.v@volantetech.com>
2025-08-19 12:23:29 +0530

    Committing 88_merge_sorted_array optimized solution

--- problems/88_merge_sorted_array/src/Solution.java
+++ problems/88_merge_sorted_array/src/Solution.java
@@ -1,5 +1,17 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = m-1, k =n-1;
+        for (int i = m+n-1; i >=0; i--) {
+            if (k<0||(j>=0&&nums1[j] > nums2[k])) {
+                nums1[i] = nums1[j];
+                j--;
+            } else if (k>=0) {
+                nums1[i] = nums2[k];
+                k--;
+            }
+        }
+    }
+    public void mergeFirstSolution(int[] nums1, int m, int[] nums2, int n) {
         int[] nums3 = new int[m+n];
         int j = 0, k = 0;
         for (int i = 0; i < m+n; i++) {
