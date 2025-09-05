7fd87914e43fb18a97b2b37f66862ef5bb2dca2a
Aranya Varatharajan <aranya.v@volantetech.com>
2025-09-05 16:41:18 +0530

    Committing 169_Majority_Element Final solution

--- problems/169_Majority_Element/src/main/java/com/array/majority/Solution.java
+++ problems/169_Majority_Element/src/main/java/com/array/majority/Solution.java
@@ -17,6 +17,14 @@ class Solution {
                 majorityCount--;
             }
         }
+        //easy suggestion
+       /* for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;*/
         return majorityElement;
     }
 
