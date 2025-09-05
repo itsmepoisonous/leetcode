1c46d1527ed519c009e4020d82afa7d35bc4a9cd
Aranya Varatharajan <aranya.v@volantetech.com>
2025-09-05 16:14:35 +0530

    Committing 169_Majority_Element Initial solution

--- problems/169_Majority_Element/src/main/java/com/array/majority/Solution.java
+++ problems/169_Majority_Element/src/main/java/com/array/majority/Solution.java
@@ -1,9 +1,22 @@
 package com.array.majority;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority = -1;
+        Map<Integer,Integer> majorityMap=new HashMap<>();
+        majorityMap.put(nums[0],1);
+        for(int i=0;i< nums.length;i++){
+        if(majorityMap.containsKey(nums[i])){
+            int a=majorityMap.get(nums[i]);
+            majorityMap.put(nums[i],a+1);
+            if(++a>=nums.length/2)
+                return nums[i];
+        }
+        else majorityMap.put(nums[i],1);
 
-        return majority;
+        }
+        return nums[0];
     }
 }
