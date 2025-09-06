13d570b570c377e8f8a5ea1e20196453db51ffc8
Aranya Varatharajan <aranya.v@volantetech.com>
2025-09-06 11:52:22 +0530

    Committing 122_Best_Time_To_Buy_And_Sell_StockII  solution

--- problems/122_Best_Time_To_Buy_And_Sell_StockII/src/main/java/com/array/maxprofit2/Solution.java
+++ problems/122_Best_Time_To_Buy_And_Sell_StockII/src/main/java/com/array/maxprofit2/Solution.java
@@ -2,9 +2,11 @@ package com.array.maxprofit2;
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        return maxProfit;
-
+        int sumMax = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) sumMax += prices[i] - prices[i - 1];
+        }
+        return sumMax;
     }
 }
 
