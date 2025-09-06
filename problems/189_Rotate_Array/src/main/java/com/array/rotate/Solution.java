dc4808779f84c74e3aa9376ea6e0b6cb24f9701b
Aranya Varatharajan <aranya.v@volantetech.com>
2025-09-06 09:07:50 +0530

    Committing 189_Rotate_Array initial  solution

--- problems/189_Rotate_Array/src/main/java/com/array/rotate/Solution.java
+++ problems/189_Rotate_Array/src/main/java/com/array/rotate/Solution.java
@@ -1,6 +1,16 @@
 package com.array.rotate;
+
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        int numLength = nums.length, i = numLength - (k % numLength);
+        if (i == numLength) i = 0;
+        if (numLength != 1) {
+            int[] rotateNums = new int[numLength];
+            for (int j = 0; j < numLength; j++, i++) {
+                if (i >= numLength) i = 0;
+                rotateNums[j] = nums[i];
+            }
+            System.arraycopy(rotateNums, 0, nums, 0, numLength);
+        }
     }
 }
